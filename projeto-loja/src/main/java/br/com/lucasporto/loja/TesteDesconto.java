26db9d94ffa0ab7aa00c14dc7531ec0732787ab7
LucasRPorto <lucasrodriguesporto@gmail.com>
2023-02-15 18:46:34 -0300

    feat(main-end-situation): Lucas Porto

--- projeto-loja/src/main/java/br/com/lucasporto/loja/TesteDesconto.java
+++ projeto-loja/src/main/java/br/com/lucasporto/loja/TesteDesconto.java
@@ -15,14 +15,17 @@ public class TesteDesconto {
         System.out.println("Imposto = "+ calculadora.calcula(orcamento, new ISS()));
 
         orcamento.aplicarDesconto();
-        System.out.println("Situação atual: "+ orcamento.getSituacao()+ " Valor: "+ orcamento.getValor());
+        System.out.println("Situação atual: "+ orcamento.getSituacao()+ "   Valor: "+ orcamento.getValor());
 
         orcamento.aprovar();
         orcamento.aplicarDesconto();
-        System.out.println("Situação atual: "+ orcamento.getSituacao()+ " Valor: "+ orcamento.getValor());
+        System.out.println("Situação atual: "+ orcamento.getSituacao()+ "   Valor: "+ orcamento.getValor());
 
-        orcamento.reprovar();
-        orcamento.aplicarDesconto();
-        System.out.println("Situação atual: "+ orcamento.getSituacao()+ " Valor: "+ orcamento.getValor());
+        orcamento.finalizar();
+        System.out.println("Situação atual: "+ orcamento.getSituacao()+ "   Valor: "+ orcamento.getValor());
+
+//        orcamento.reprovar();
+//        orcamento.aplicarDesconto();
+//        System.out.println("Situação atual: "+ orcamento.getSituacao()+ " Valor: "+ orcamento.getValor());
     }
 }
